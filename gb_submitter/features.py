f574a780dbcd2fe6f1cb0e338701d5c14a0ec21c
LanderDC <lander.deconinck@kuleuven.be>
2025-02-11 16:56:50 +0100

    include reverse complementation for negarnaviricota

--- gb_submitter/features.py
+++ gb_submitter/features.py
@@ -1,18 +1,22 @@
-#TODO: make output path if doesnt exist 
-#TODO: add help info
-#TODO: do not reverse complement when negarnaviricota ==> see taxonomy script(?)
+# TODO: make output path if doesnt exist
+# TODO: add help info
+# TODO: do not reverse complement when negarnaviricota ==> see taxonomy script(?)
 import os
 
-import click
 import Bio.SeqIO
+import click
 import pandas as pd
 import pyrodigal_gv
+import taxopy
 from Bio.SeqIO import write
+
 from gb_submitter import utils
 
+
 def calculate_coding_capacity(genes, seq_length):
-        """Calculate the total coding capacity for a list of genes."""
-        return sum((gene.end - gene.begin) / seq_length for gene in genes)
+    """Calculate the total coding capacity for a list of genes."""
+    return sum((gene.end - gene.begin) / seq_length for gene in genes)
+
 
 def find_orientation(genes):
     """
@@ -25,7 +29,8 @@ def find_orientation(genes):
     int: The sum of strand orientations across all genes, where each strand is typically represented as 1 (forward) or -1 (reverse).
     """
     return sum(gene.strand for gene in genes)
-    
+
+
 def extract_gene_results(genes, record_id, seq_length):
     """Extract gene prediction results for a sequence."""
     return [
@@ -38,10 +43,12 @@ def extract_gene_results(genes, record_id, seq_length):
             gene.strand,
             gene.start_node.type,
             gene.partial_begin,
-            gene.partial_end
+            gene.partial_end,
         ]
         for i, gene in enumerate(genes)
     ]
+
+
 def write_proteins(genes, record_id, dst_path, overwrite):
     """Write protein translations to a file."""
     with open(dst_path, "w" if overwrite else "a") as dst:
@@ -53,11 +60,14 @@ def write_proteins(genes, record_id, dst_path, overwrite):
             include_stop=False,
         )
     return False  # Update overwrite flag
+
+
 def write_nucleotides(sequence, output_handle, overwrite):
-    """Write protein translations to a file."""
+    """Write nucleotide sequences to a file."""
     with open(output_handle, "w" if overwrite else "a") as dst:
         write(sequence, dst, "fasta")
     return False
+
 
 def select_top_structure(df):
     """
@@ -77,6 +87,19 @@ def select_top_structure(df):
     # Select those rows
     result = df.loc[highest_bits_idx]
     return result
+
+
+def read_taxonomy_table(taxonomy):
+    taxonomy_data = None
+    if taxonomy:
+        taxonomy_data = pd.read_csv(taxonomy, sep="\t")
+    return taxonomy_data
+
+
+def retrieve_lineage(taxid, taxdb):
+    lineage = taxopy.Taxon(taxid, taxdb).name_lineage
+    return lineage
+
 
 # Function to generate and save NCBI feature tables
 def save_ncbi_feature_tables(df, output_dir="./"):
@@ -104,17 +127,17 @@ def save_ncbi_feature_tables(df, output_dir="./"):
         with open(filename, "w") as file:
             file.write(f">Feature {accession}\n")
             for _, row in group.iterrows():
-                end = row['end']
-                start = row['start']
-                if row['partial_end']:
+                end = row["end"]
+                start = row["start"]
+                if row["partial_end"]:
                     # This should not be possible with extended pyrodigal enforcing start codon (?)
-                    if row['strand'] == -1:
+                    if row["strand"] == -1:
                         end = f"<{row['end']}"
                     # This is fine
                     else:
                         end = f">{row['end']}"
-                elif row['partial_begin']:
-                    if row['strand'] == -1:
+                elif row["partial_begin"]:
+                    if row["strand"] == -1:
                         start = f">{row['start']}"
 
                 if row["strand"] == -1:
@@ -135,32 +158,88 @@ def save_ncbi_feature_tables(df, output_dir="./"):
                         f"\t\t\tnote\tAlternative start codon: {row['start_codon']}\n"
                     )
                 if protein != "hypothetical protein":
-                    #file.write(f"\t\t\tnote\tAnnotation database: {row['annotation_source']}\n")
-                    file.write((f"\t\t\tinference\talignment:{row['aligner']}:{row['aligner_version']}:UniProtKB:{row['Uniref_entry']},BFVD:{row['model']}\n"))
+                    # file.write(f"\t\t\tnote\tAnnotation database: {row['annotation_source']}\n")
+                    file.write(
+                        (
+                            f"\t\t\tinference\talignment:{row['aligner']}:{row['aligner_version']}:UniProtKB:{row['Uniref_entry']},BFVD:{row['model']}\n"
+                        )
+                    )
 
         print(f"Saved: {filename}")
 
+
 @click.command(help="Create feature tables for sequences.")
-@click.option('-i', '--input-file', 'fasta_file', required=True, type=click.Path(exists=True), help='Input fasta file')
-@click.option('-o', '--output-path', 'output_path', required=True, type=click.Path(exists=False), help='Output directory')
-@click.option('-d', '--database', 'database', required=True, type=click.Path(exists=True), help='BFVD diamond database path')
-#TODO: limit genetic codes?
-@click.option('-g', '--translation-table', 'transl_table', required=False, type=click.Choice(list(range(1, 32))), default=1, metavar="<1-31>", help='Translation table to use')
-@click.option('-t', '--threads', 'threads', required=False, default=4, type=int, help='Number of threads to use')
-def features(fasta_file, output_path, database, transl_table, threads):
+@click.option(
+    "-i",
+    "--input-file",
+    "fasta_file",
+    required=True,
+    type=click.Path(exists=True),
+    help="Input fasta file",
+)
+@click.option(
+    "-o",
+    "--output-path",
+    "output_path",
+    required=True,
+    type=click.Path(exists=False),
+    help="Output directory",
+)
+@click.option(
+    "-d",
+    "--database",
+    "database",
+    required=True,
+    type=click.Path(exists=True),
+    help="BFVD diamond database path",
+)
+# TODO: limit genetic codes?
+@click.option(
+    "-g",
+    "--translation-table",
+    "transl_table",
+    required=False,
+    type=click.Choice(list(range(1, 32))),
+    default=1,
+    metavar="<1-31>",
+    help="Translation table to use",
+)
+@click.option(
+    "--taxonomy",
+    required=False,
+    type=click.Path(exists=True),
+    help="Taxonomy file",
+)
+@click.option(
+    "-t",
+    "--threads",
+    "threads",
+    required=False,
+    default=4,
+    type=int,
+    help="Number of threads to use",
+)
+def features(fasta_file, output_path, database, transl_table, taxonomy, threads):
     records = list(Bio.SeqIO.parse(fasta_file, "fasta"))
 
     orf_finder = pyrodigal_gv.ViralGeneFinder()
-    training_info = orf_finder.train(*(bytes(seq.seq) for seq in records), translation_table=transl_table)
-
+    training_info = orf_finder.train(
+        *(bytes(seq.seq) for seq in records), translation_table=transl_table
+    )
 
     orf_finder = pyrodigal_gv.ViralGeneFinder(
         meta=False, viral_only=True, closed=True, training_info=training_info
     )
 
     orf_finder2 = pyrodigal_gv.ViralGeneFinder(
-        meta=False, viral_only=True, closed=[True,False], training_info=training_info
+        meta=False, viral_only=True, closed=[True, False], training_info=training_info
     )
+
+    taxdb = taxopy.TaxDb(
+        nodes_dmp="/lustre1/scratch/337/vsc33750/ictv_db/ictv_taxdump/nodes.dmp",
+        names_dmp="/lustre1/scratch/337/vsc33750/ictv_db/ictv_taxdump/names.dmp",
+    )  # TODO set database, provide dmp files
+    taxonomy_data = read_taxonomy_table(taxonomy)
 
     prot_path = f"{output_path}/proteins.faa"
     nucl_path = f"{output_path}/reoriented_nucleotide_sequences.fna"
@@ -171,6 +250,13 @@ def features(fasta_file, output_path, database, transl_table, threads):
     for record in records:
         seq_length = len(record.seq)
 
+        lineage = []
+        if taxonomy:
+            record_taxonomy = taxonomy_data[taxonomy_data["contig"] == record.id]
+            taxid_dict = record_taxonomy.set_index("contig")["taxid"].to_dict()
+            # print(taxid_dict[record.id])
+            lineage = retrieve_lineage(taxid_dict[record.id], taxdb)
+
         seq_results = []
 
         genes = orf_finder.find_genes(bytes(record.seq))
@@ -178,7 +264,10 @@ def features(fasta_file, output_path, database, transl_table, threads):
         orientation = find_orientation(genes)
 
         if coding_capacity > 0.5:
-            if orientation < 0:
+            if orientation < 0 and "Negarnaviricota" not in lineage:
+                record.seq = record.seq.reverse_complement()
+                genes = orf_finder.find_genes(bytes(record.seq))
+            elif orientation > 0 and "Negarnaviricota" in lineage:
                 record.seq = record.seq.reverse_complement()
                 genes = orf_finder.find_genes(bytes(record.seq))
             seq_results = extract_gene_results(genes, record.id, seq_length)
@@ -191,7 +280,10 @@ def features(fasta_file, output_path, database, transl_table, threads):
             orientation = find_orientation(genes)
 
             if coding_capacity > 0.5:
-                if orientation < 0:
+                if orientation < 0 and "Negarnaviricota" not in lineage:
+                    record.seq = record.seq.reverse_complement()
+                    genes = orf_finder2.find_genes(bytes(record.seq))
+                elif orientation > 0 and "Negarnaviricota" in lineage:
                     record.seq = record.seq.reverse_complement()
                     genes = orf_finder2.find_genes(bytes(record.seq))
                 seq_results = extract_gene_results(genes, record.id, seq_length)
@@ -204,52 +296,61 @@ def features(fasta_file, output_path, database, transl_table, threads):
                 )
 
         results.extend(seq_results)
-    
-    with open(f'{output_path}/no_ORF_prediction.txt', 'w') as f:
+
+    with open(f"{output_path}/no_ORF_prediction.txt", "w") as f:
         for line in no_orf_pred:
             f.write(f"{line}\n")
 
-
     # Create DataFrame from results
-    columns = ["seqid", "seq_length", "orf", "start", "end", "strand", "start_codon", "partial_begin", "partial_end"]
+    columns = [
+        "seqid",
+        "seq_length",
+        "orf",
+        "start",
+        "end",
+        "strand",
+        "start_codon",
+        "partial_begin",
+        "partial_end",
+    ]
     df = pd.DataFrame(results, columns=columns)
 
     df["seqid"] = df["seqid"].str.strip()
     df["type"] = "CDS"
     df["source"] = f"pyrodigal-gv:{pyrodigal_gv.__version__}"
-    #df["source"] = f"pyrodigal-gv"
-    #df["annotation_source"]=f"BFVD (https://doi.org/10.1093/nar/gkae1119)"
-    df["annotation_source"]="UniProtKB"
+    # df["source"] = f"pyrodigal-gv"
+    # df["annotation_source"]=f"BFVD (https://doi.org/10.1093/nar/gkae1119)"
+    df["annotation_source"] = "UniProtKB"
 
-    #Cmd = "diamond blastp "
-    #Cmd += f"--db {database}/foldseek_db/bfvd.dmnd "
-    #Cmd += f"--query {output_path}/proteins.faa "
-    #Cmd += f"--out {output_path}/gb_sub_proteins.m8 "
-    #Cmd += f"--threads {threads} "
-    #Cmd += "--sensitive "
-    #Cmd += "--index-chunks 1 "
-    #Cmd += "--block-size 8 "
-    #Cmd += "--unal 1 "
-    #Cmd += "--tmpdir /dev/shm "
-    #Cmd += "--outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
-    #utils.Exec(Cmd)
-#
-    #aligner = "Diamond"
-    #aligner_version = utils.Exec("diamond version", capture=True)
-    #aligner_version = aligner_version.strip().split()[2]
+    # Cmd = "diamond blastp "
+    # Cmd += f"--db {database}/foldseek_db/bfvd.dmnd "
+    # Cmd += f"--query {output_path}/proteins.faa "
+    # Cmd += f"--out {output_path}/gb_sub_proteins.m8 "
+    # Cmd += f"--threads {threads} "
+    # Cmd += "--sensitive "
+    # Cmd += "--index-chunks 1 "
+    # Cmd += "--block-size 8 "
+    # Cmd += "--unal 1 "
+    # Cmd += "--tmpdir /dev/shm "
+    # Cmd += "--outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
+    # utils.Exec(Cmd)
+    #
+    # aligner = "Diamond"
+    # aligner_version = utils.Exec("diamond version", capture=True)
+    # aligner_version = aligner_version.strip().split()[2]
 
     Cmd = "mmseqs easy-search "
-    Cmd += f"{output_path}/proteins.faa " #input
-    Cmd += f"{database}/foldseek_db/bfvd" # database
-    Cmd += f"{output_path}/gb_sub_proteins.m8 " #output
-    Cmd += "tmp " #temp directory
+    Cmd += f"{output_path}/proteins.faa "  # input
+    Cmd += f"{database}/foldseek_db/bfvd"  # database
+    Cmd += f"{output_path}/gb_sub_proteins.m8 "  # output
+    Cmd += "tmp "  # temp directory
     Cmd += "-s 7.5 "
     Cmd += "--format-mode 4 "
     Cmd += "--format-output query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits "
     Cmd += f"--threads {threads}"
 
     aligner = "MMseqs2"
-    aligner_version = utils.Exec("mmseqs version", capture=True)
+    aligner_version = utils.Exec("mmseqs version", capture=True).strip()
 
     m8 = pd.read_csv("gb_sub_proteins.m8", sep="\t", header=None)
     m8.rename(
@@ -271,19 +372,24 @@ def features(fasta_file, output_path, database, transl_table, threads):
         inplace=True,
     )
 
+    m8 = m8[m8["evalue"] < 1e-3]
 
-    m8=m8[m8["evalue"]<1e-3]
-
-    m8['aligner'] = aligner
-    m8['aligner_version'] = aligner_version
+    m8["aligner"] = aligner
+    m8["aligner_version"] = aligner_version
 
     m8_top = select_top_structure(m8)
-    names_df = pd.read_csv(f"{database}/bfvd_uniprot_names.tsv", sep="\t")
+    names_df = pd.read_csv(
+        f"{database}_uniprot_names.tsv", sep="\t"
+    )  # TODO find better solution
 
     # remove all trailing strings within brackets from protein names
-    names_df['Protein names'] = names_df['Protein names'].str.replace(r'[\(\[].*?[\)\]]$', '', regex=True)
+    names_df["Protein names"] = names_df["Protein names"].str.replace(
+        r"[\(\[].*?[\)\]]$", "", regex=True
+    )
 
-    meta_df = pd.read_csv(f"{database}/bfvd_metadata.tsv", sep="\t", header=None)
+    meta_df = pd.read_csv(
+        f"{database}_metadata.tsv", sep="\t", header=None
+    )  # TODO find better solution
     meta_df.rename(
         {
             0: "Uniref_entry",
@@ -303,20 +409,25 @@ def features(fasta_file, output_path, database, transl_table, threads):
         meta_df, names_df, left_on="Uniref_entry", right_on="From", how="left"
     )
 
-    prot_df = pd.merge(m8_top, merged_df, left_on="target", right_on="model", how="left")
+    prot_df = pd.merge(
+        m8_top, merged_df, left_on="target", right_on="model", how="left"
+    )
 
-    #prot_df["Protein names"]
+    # prot_df["Protein names"]
 
-    prot_df.to_csv(f"{output_path}/diamond_names.tsv", sep="\t", index=False)
+    prot_df.to_csv(
+        f"{output_path}/diamond_names.tsv", sep="\t", index=False  # TODO change name
+    )
 
     diamond = prot_df
-    final_df = pd.merge(df, diamond, left_on="orf", right_on="query", how="left")  
-    
+    final_df = pd.merge(df, diamond, left_on="orf", right_on="query", how="left")
+
     # Call the function to save feature tables
     save_ncbi_feature_tables(
         final_df,
         output_dir=f"{output_path}/feature_tables/",
     )
+
 
 if __name__ == "__main__":
     features()
