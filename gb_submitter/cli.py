01dd52ee3941f45cdd0fb2f2511a9fac9466235e
LanderDC <lander.deconinck@kuleuven.be>
2025-02-12 16:49:30 +0100

    format help

--- gb_submitter/cli.py
+++ gb_submitter/cli.py
@@ -1,50 +1,47 @@
-#TODO: format help to show everything
+from gettext import gettext as _
+
 import click
+
 from gb_submitter.features import features
 from gb_submitter.gbk2tbl import gbk2tbl
 from gb_submitter.taxonomy import taxonomy
 
-from gettext import gettext as _
 
 class FullHelpGroup(click.Group):
-    def format_commands(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
-        """Extra format methods for multi methods that adds all the commands
-        after the options.
-        """
-        commands = []
-        for subcommand in self.list_commands(ctx):
-            cmd = self.get_command(ctx, subcommand)
-            # What is this, the tool lied about a command.  Ignore it
-            if cmd is None:
-                continue
-            if cmd.hidden:
-                continue
+    def list_commands(self, ctx: click.Context):
+        """Return commands in the order they were added."""
+        return list(self.commands.keys())
 
-            commands.append((subcommand, cmd))
+    def format_commands(self, ctx: click.Context, formatter: click.HelpFormatter):
+        """Formats and displays commands in the correct order."""
+        commands = [
+            (name, self.get_command(ctx, name))
+            for name in self.list_commands(ctx)
+            if self.get_command(ctx, name) and not self.get_command(ctx, name).hidden
+        ]
 
-        # allow for 3 times the default spacing
-        if len(commands):
-            limit = formatter.width - 6 - max(len(cmd[0]) for cmd in commands)
+        if commands:
+            rows = [(name, cmd.help or "") for name, cmd in commands]
 
-            rows = []
-            for subcommand, cmd in commands:
-                help = cmd.help if cmd.help is not None else ""
-                rows.append((subcommand, help))
+            with formatter.section(_("Commands")):
+                formatter.write_dl(rows)
 
-            if rows:
-                with formatter.section(_("Commands")):
-                    formatter.write_dl(rows)
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'], show_default=True)
+CONTEXT_SETTINGS = dict(
+    help_option_names=["-h", "--help"], show_default=True, max_content_width=120
+)
+
 
 @click.group(context_settings=CONTEXT_SETTINGS, cls=FullHelpGroup)
 @click.version_option()
 def cli():
     """Tool to submit viral sequences to Genbank."""
 
-cli.add_command(gbk2tbl)
-cli.add_command(features)
+
 cli.add_command(taxonomy)
+cli.add_command(features)
+cli.add_command(gbk2tbl)
+
 
 if __name__ == "__main__":
     cli()
