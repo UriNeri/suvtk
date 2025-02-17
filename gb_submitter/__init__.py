d34fc13ccad81ffcc2c93b9a45cea674c5c84c1e
LanderDC <75493579+LanderDC@users.noreply.github.com>
2025-02-17 16:49:27 +0100

    add table2asn command

--- gb_submitter/__init__.py
+++ gb_submitter/__init__.py
@@ -2,3 +2,4 @@ from .features import features
 from .gbk2tbl import gbk2tbl
 from .taxonomy import taxonomy
 from .co_occurrence import co_occurrence
+from .table2asn import table2asn
---
+++ gb_submitter/table2asn.py
@@ -0,0 +1,57 @@
+import click
+import os
+from gb_submitter import utils
+
+
+@click.command(help="Generate .sqn submission for Genbank.")
+@click.option(
+    "-i",
+    "--input",
+    "input",
+    required=True,
+    type=click.Path(exists=True),
+    help="Input fasta file",
+)
+@click.option(
+    "-o",
+    "--output",
+    "output",
+    required=True,
+    type=click.Path(exists=False),
+    help="Output prefix",
+)
+@click.option(
+    "-s",
+    "--src-file",
+    "src_file",
+    required=True,
+    type=click.Path(exists=True),
+    help="File with Source modifiers (.src).",
+)
+@click.option(
+    "-f",
+    "--features",
+    "features",
+    required=True,
+    type=click.Path(exists=True),
+    help="Feature table file (.tbl).",
+)
+@click.option(
+    "-t",
+    "--template",
+    "template",
+    required=True,
+    type=click.Path(exists=True),
+    help="Template file with author information (.sbt). See https://submit.ncbi.nlm.nih.gov/genbank/template/submission/",
+)
+def table2asn(input, output, src_file, features, template):
+    Cmd = "table2asn "
+    Cmd += f"-i {input} "
+    Cmd += f"-o {output}.sqn "
+    Cmd += f"-t {template} "
+    Cmd += f"-f {features} "
+    Cmd += f"-src-file {src_file} "
+    Cmd += "-V v "  # Check for errors
+    Cmd += "-a s"  # allow multifasta file
+
+    utils.Exec(Cmd)
