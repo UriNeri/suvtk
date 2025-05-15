3d551136f8013caaf11b2d2e0d108d65b9255896
LanderDC <75493579+LanderDC@users.noreply.github.com>
2025-05-15 14:33:51 +0100

    update database

--- suvtk/download_database.py
+++ suvtk/download_database.py
@@ -30,7 +30,7 @@ import click
 import requests
 
 # The Zenodo DOI is fixed
-ZENODO_DOI = "10.5281/zenodo.15374440"
+ZENODO_DOI = "10.5281/zenodo.15423947"
 
 
 def doi_to_record_id(doi: str) -> str:
